444a87b307745b9618bd063692d731589dd36fc8
ispan <ispan@DESKTOP-PAKSETB>
2022-11-15 15:52:10 +0800

    加入專案檔案。

---
+++ 利用委派達到篩選集合的目的/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 利用委派達到篩選集合的目的
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> items = new List<int> { 88, 5, 4,17, 65 };
+			Func<int, bool> isEven = n => n % 2 == 0;
+
+			List<int> EvenItems = GetEvenItems(items, isEven);
+			foreach (var item in EvenItems)
+			{
+				Console.WriteLine(item);
+			}
+
+		}
+
+		static List<int> GetEvenItems(List<int> source, Func<int, bool> func)
+		{
+			var result = new List<int>();
+
+			foreach (int item in source)
+			{
+
+				Console.WriteLine(func(item));
+				
+				if (func(item) == true)
+				{
+					result.Add(item);
+				}
+			}
+
+			return result;
+		}
+	}
+}
